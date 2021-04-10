f06c65ea5e7259be610e82c1cbb66f05eccf5a25
ko1219 <73377107+ko1219@users.noreply.github.com>
2021-04-10 13:42:58 +0900

    Portfolio Component Update & Completed

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -2,7 +2,7 @@ import React from 'react';
 import chatApp from '../images/chat-app.png';
 import choiceClass from '../images/choice-class.png';
 import furimaApp from '../images/furima-app.png';
-import pictweet from '../images/pictweet.png';
+import protoSpace from '../images/proto-space.png';
 // FONTAWESOME IMPORTS
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faSearchPlus } from '@fortawesome/free-solid-svg-icons';
@@ -35,25 +35,72 @@ const Portfolio = () => {
   }
 
   // ChoiceClass App
-  const openPopupboxChatApp = () => {
+  const openPopupboxChoiceClass = () => {
     const content = (
       <>
-      <img className="portfolio-image-popupbox" src={choiceClass} alt="Review curriculums application..."/>
+      <img className="portfolio-image-popupbox" src={choiceClass} alt="Review Tokoha university's curriculums application..."/>
       <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Corporis quos amet perspiciatis nihil alias quis temporibus asperiores. Pariatur, quam laudantium?</p>
-      <b>GitHub:</b> <a className="hyper-link" onClick={() => window.open("https://github.com/ko1219/chat-app")}>https://github.com/ko1219/chat-app</a>
+      <b>GitHub:</b> <a className="hyper-link" onClick={() => window.open("https://github.com/ko1219/choice-class")}>https://github.com/ko1219/choice-class</a>
       </>
     )
       PopupboxManager.open({ content })
   }
 
-  const popupboxConfigChatApp = {
+  const popupboxConfigChoiceClass = {
     titleBar: {
       enable: true,
-      text: "Chat application."
+      text: "Review Tokoha university's curriculums application."
     },
     fadeIn: true,
     fadeInSpeed: 500
   }
+
+  // FurimaApp
+  const openPopupboxFurimaApp = () => {
+    const content = (
+      <>
+      <img className="portfolio-image-popupbox" src={furimaApp} alt="Free market application..."/>
+      <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Corporis quos amet perspiciatis nihil alias quis temporibus asperiores. Pariatur, quam laudantium?</p>
+      <b>Demo:</b> <a className="hyper-link" onClick={() => window.open("https://furima-34865.herokuapp.com/", "_blank")}>https://furima-34865.herokuapp.com/</a>
+      <br/>
+      <b>GitHub:</b> <a className="hyper-link" onClick={() => window.open("https://github.com/ko1219/furima-34865")}>https://github.com/ko1219/furima-34865</a>
+      </>
+    )
+      PopupboxManager.open({ content })
+  }
+
+  const popupboxConfigFurimaApp = {
+    titleBar: {
+      enable: true,
+      text: "Free market application."
+    },
+    fadeIn: true,
+    fadeInSpeed: 500
+  }
+
+  // Protospace
+  const openPopupboxProtoSpace = () => {
+    const content = (
+      <>
+      <img className="portfolio-image-popupbox" src={protoSpace} alt="Upload your portfolio..."/>
+      <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Corporis quos amet perspiciatis nihil alias quis temporibus asperiores. Pariatur, quam laudantium?</p>
+      <b>Demo:</b> <a className="hyper-link" onClick={() => window.open("https://protospace-34865.herokuapp.com/", "_blank")}>https://protospace-34865.herokuapp.com/</a>
+      <br/>
+      <b>GitHub:</b> <a className="hyper-link" onClick={() => window.open("https://github.com/ko1219/protospace-34865")}>https://github.com/ko1219/protospace-34865</a>
+      </>
+    )
+      PopupboxManager.open({ content })
+  }
+
+  const popupboxConfigProtoSpace = {
+    titleBar: {
+      enable: true,
+      text: "Upload your portfolio."
+    },
+    fadeIn: true,
+    fadeInSpeed: 500
+  }
+
 
   return (
     <div className="portfolio-wrapper">
@@ -66,26 +113,29 @@ const Portfolio = () => {
             <FontAwesomeIcon className="portfolio-icon" icon={faSearchPlus} />
           </div>
           {/* - */}
-          <div className="portfolio-image-box">
+          <div className="portfolio-image-box" onClick={openPopupboxChoiceClass}>
             <img className="portfolio-image " src={choiceClass} alt="Review curriculums application..."/>
             <div className="overflow"></div>
             <FontAwesomeIcon className="portfolio-icon" icon={faSearchPlus} />
           </div>
           {/* - */}
-          <div className="portfolio-image-box">
+          <div className="portfolio-image-box" onClick={openPopupboxFurimaApp}>
             <img className="portfolio-image " src={furimaApp} alt="Free market application..."/>
             <div className="overflow"></div>
             <FontAwesomeIcon className="portfolio-icon" icon={faSearchPlus} />
           </div>
           {/* - */}
-          <div className="portfolio-image-box">
-            <img className="portfolio-image " src={pictweet} alt="Tweet application..."/>
+          <div className="portfolio-image-box" onClick={openPopupboxProtoSpace}>
+            <img className="portfolio-image " src={protoSpace} alt="Upload your portfolio..."/>
             <div className="overflow"></div>
             <FontAwesomeIcon className="portfolio-icon" icon={faSearchPlus} />
           </div>
         </div>
       </div>
       <PopupboxContainer {...popupboxConfigChatApp}/>
+      <PopupboxContainer {...popupboxConfigChoiceClass}/>
+      <PopupboxContainer {...popupboxConfigFurimaApp}/>
+      <PopupboxContainer {...popupboxConfigProtoSpace}/>
     </div>
   )
 }
